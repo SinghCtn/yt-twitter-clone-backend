87165980f11bbb998564a78e96d2209b65037f97
chetan <singhctn2001@gmail.com>
2024-05-16 14:15:40 +0530

    added models
    Added the user models and vedio models

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "16kb" })); //configuring the size of json being a
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //used to understand the encoded url's like %20 or + in between the url text and extended true allows us to parse objects inside objects
 
-app.use(express.static("public")); // this configuration is allowing us to store the static data (images, pdf, etc.)in the public folder
+app.use(express.static("public")); // this configuration is allowing us to store the static data (images, pdf, etc.)in the public folder(mostly temporaroly)
 
 app.use(cookieParser()); // this cookie-parser allows us to interact with the cookies that are securely stored on the user's browser
 
