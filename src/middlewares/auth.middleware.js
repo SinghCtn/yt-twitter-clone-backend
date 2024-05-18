f939e30b2e693ccf05ab7cbe13362746ce353a72
chetan <singhctn2001@gmail.com>
2024-05-18 16:58:24 +0530

    Add user account update endpoints

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -13,11 +13,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       throw new ApiError(401, "Unauthorized request");
     }
 
-    console.log(token);
-
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    console.log(decodedToken);
 
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
